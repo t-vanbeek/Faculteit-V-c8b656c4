b42b9facfb772d3e69420e5d21a2ac710c3ba079
Thorvald van Beek <54794887+t-vanbeek@users.noreply.github.com>
2019-09-25 10:49:33 +0200

    Uitwerking van de exercise

---
+++ faculteit_v2.php
@@ -0,0 +1,14 @@
+<?php
+
+$getall = array(
+    $getal = readline('Van welk getal wil je de faculiteit weten?')
+);
+
+for ($faculiteit = $getal; $faculiteit <= $getal; $faculiteit++) {
+    print 'De faculiteit van ' . $getal . ' is ' ;
+};
+$factuliteit=1;
+$i=2;
+while($i <= $getal){
+$factuliteit *= $i++;};
+    echo $factuliteit;
